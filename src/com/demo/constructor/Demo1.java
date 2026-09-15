8e90ddcf0c01e46bbb11e0cd7449ad05b7afe34e
Munja Sakhare <154612218+munjasakhare9@users.noreply.github.com>
2026-09-15 13:39:06 +0530

    contructor

---
+++ src/com/demo/constructor/Demo1.java
@@ -0,0 +1,15 @@
+package com.demo.constructor;
+
+public class Demo1 {
+	public Demo1() {
+        System.out.println("This is the actual constructor.");
+    }
+
+	public void Demo1() {
+		System.out.println("This is a Demo1 method.");
+	}
+
+	void display() {
+		System.out.println("display method of Demo1");
+	}
+}
---
+++ src/com/demo/constructor/MainClass.java
@@ -0,0 +1,9 @@
+package com.demo.constructor;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Demo1 d1=new Demo1();
+		d1.Demo1();
+		d1.display();
+	}
+}
